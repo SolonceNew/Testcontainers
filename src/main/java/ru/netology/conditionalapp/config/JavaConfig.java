828a968136e76da5e209ebcba8c79fe15f8d5657
Anna Fabrichnaya <fabrichnaya.anna@bk.ru>
2022-04-05 20:33:33 +0300

    matchMissing

--- src/main/java/ru/netology/conditionalapp/config/JavaConfig.java
+++ src/main/java/ru/netology/conditionalapp/config/JavaConfig.java
@@ -11,13 +11,14 @@ import ru.netology.conditionalapp.profiles.SystemProfile;
 public class JavaConfig {
 
     @Bean
-    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "true")
+    @ConditionalOnProperty(value = "netology.profile.dev", matchIfMissing = true,
+            havingValue = "true")
     public SystemProfile devProfile() {
         return new DevProfile();
     }
 
     @Bean
-    @ConditionalOnProperty(value = "netology.profile.dev",havingValue = "false")
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "false")
     public SystemProfile prodProfile() {
         return new ProductionProfile();
     }
